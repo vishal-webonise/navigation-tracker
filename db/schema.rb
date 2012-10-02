3d8ef117a05f7446ec299d5258d0d2fe63d9d2c5
naina <naina1290@gmail.com>
2012-10-02 11:06:59 +0530

    Create views for index & edit

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20121001053720) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password"
     t.string   "login_type"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
@@ -57,5 +58,12 @@ ActiveRecord::Schema.define(:version => 20121001053720) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "users_projects", :id => false, :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
