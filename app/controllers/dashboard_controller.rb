57032cbf0d1267625d0d1fb58962def4d73602df
Vishal Telangre <vishal@weboniselab.com>
2012-10-03 19:17:56 +0530

    Add form to create project on user dashboard#index page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,14 @@
 class DashboardController < ApplicationController
   def index
   	@user = current_user
-  	@projects = @user.projects.build
+  	@project = @user.projects.build
+  end
+
+  def create_project
+  	@project = current_user.projects.build(params[:project])
+    if @project.save
+      flash[:success] = "Project created successfully"
+      redirect_to :back
+    end
   end
 end
