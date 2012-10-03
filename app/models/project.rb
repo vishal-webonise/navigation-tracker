5cbe391b374a211c144f5519bc5d505409c1c8cd
Nayana Gharte <nayana@weboniselab.com>
2012-10-03 11:46:03 +0530

    Modify views of show & index

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   attr_accessible :domain, :git_url, :name, :project_mgt_tool
   has_and_belongs_to_many :users
   has_many :analytic_data
+
+  validates :name, :presence => true ,:length=> { :maximum => 50 }
 end
