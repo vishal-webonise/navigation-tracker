56bccd22e148d3a80d9a1d2b36e4a5312541c119
Vishal Telangre <vishal@weboniselab.com>
2012-10-03 10:29:33 +0530

    Show profile of individual user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
 	before_filter :authenticate_user!
-	before_filter :is_admin?, only: [:index]
-	
-	def index
+	before_filter :user_signed_in?
+	before_filter :admin_user, only: [:index]
 
+	def index
+		@users = User.all
 	end
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+
+		def admin_user
+      		redirect_to(user_path(current_user)) unless is_admin?
+    	end
 end
