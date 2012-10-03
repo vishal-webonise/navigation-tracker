fc70a2a6acfecc1b28d27b0d43bbf98c88e10953
Vishal Telangre <vishal@weboniselab.com>
2012-10-03 11:13:03 +0530

    Add actions to user model to delete the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
 	before_filter :authenticate_user!
 	before_filter :user_signed_in?
-	before_filter :admin_user, only: [:index]
+	before_filter :admin_user, only: [:index, :destroy]
 
 	def index
 		@users = User.all
 	end
+	
 	def show
 		@user = User.find(params[:id])
+	end
+
+	def destroy
+		@user = User.find(params[:id]).destroy
+		flash[:success] = "User --> #{@user.name} is successfully deleted!"
+		redirect_to :back
 	end
 
 	private
