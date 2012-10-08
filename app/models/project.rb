465c2906c19ac61f334aea45b24ac1dd35d3bfd3
Nayana Gharte <nayana@weboniselab.com>
2012-10-08 13:23:00 +0530

    Add custom validations for user & project model and add delete option for project in user dashboard

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,10 +2,20 @@ class Project < ActiveRecord::Base
   attr_accessible :name, :domain_name, :domain_url
   self.per_page = 10
 
-  validates :name, :domain_name, :domain_url, :presence => true
-  validates :name, :length=> { :maximum => 50 }
-  validates :name, :domain_name, :domain_url, :uniqueness => true
-  validates_format_of :domain_url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i
+  #validates :name, :domain_name, :domain_url, :presence => true
+  validates :name ,:presence => { :message => "Project name can't be blank" }
+  validates_length_of :name, :maximum => 50, :message=>"Name can't be more than 50 characters"
+  validates :domain_name ,:presence => { :message => "Domain name can't be blank" }
+  validates :domain_url ,:presence => { :message => "Domain URL can't be blank" }
+  #validates :name, :length=> { :maximum => 50 , :message => "Name can't be more than 50 characters" }
+
+
+
+  validates :name, :uniqueness => { :message => "Project name should be unique" }
+  validates :domain_name, :uniqueness => { :message => "Domain name should be unique" }
+  validates :domain_url, :uniqueness => { :message => "Domain URL should be unique" }
+
+  validates_format_of :domain_url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i, :message => "Enter a valid domain URL"
 
 	has_many :user_projects, :dependent => :destroy
 	has_many :users, :through => :user_projects, :uniq => true
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,19 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :login_type, :first_name, :last_name, :encrypted_password, :current_password
 
-  validates :first_name, :last_name, :email, presence: true
-  if :password.present? do
-    validates :password
-    validates :password, length: { minimum: 6, maximum: 15 }
-    validates_format_of :password, :with => /^[a-zA-Z0-9]+$/
-  end
-  end
+  #validates :first_name, :email, presence: true
+  validates :first_name, :presence => { :message => "User first name can't be blank" }
+  validates :last_name, :presence => { :message => "User last name can't be blank" }
+  validates :email, :presence => { :message => "Email can't be blank" }
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Enter a valid email address"
+  validates :email, :uniqueness => { :message => "Email should be unique" }
 
-  validates_uniqueness_of :email, :on => :create
+
+  if :password.present? do
+    validates :password, length: { minimum: 6, maximum: 15 }, :message => "Enter password between 6 to 15 characters"
+    validates_format_of :password, :with => /^[a-zA-Z0-9]+$/, :message => "Enter password in alphanumeric format"
+  end
+  end
 
   has_many :user_projects, :dependent => :destroy
   has_many :projects, :through => :user_projects, :uniq => true
