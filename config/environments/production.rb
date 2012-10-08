b2636f03aeddc77aed82fdc6cbd90f0b17b4946e
Vishal Telangre <vishal@weboniselab.com>
2012-10-08 10:53:17 +0530

    Add global variable named SITE_HOST in both development and production environment configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 NavigationTracker::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Constants
+  SITE_HOST = "navigation_tracking.weboapps.com"
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -64,4 +67,7 @@ NavigationTracker::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Devise: action mailer default url
+  config.action_mailer.default_url_options = { :host => SITE_HOST }
 end
